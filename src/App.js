f83f6416672cf1f92ce21511ab37939a2a23741c
younes <younes71682@gmail.com>
2024-08-09 10:38:36 +0330

    fix main js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './project-Store/pages/HomePage';
-import CartPage from './project-Store/pages/CartPage';
-import Navbar from './project-Store/components/layout/Navbar';
-import store from './project-Store/redux/store';
+import Main from "./project-Store/Main";
 
 
-const App = () => {
+
+function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Main />
   );
-};
+}
 
 export default App;
